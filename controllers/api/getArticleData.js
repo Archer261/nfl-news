e0e9baf00d1cbc254880d79d784ad539694a5a19
Archer <josepharcher@rockethomes.com>
2022-09-07 14:45:42 -0400

    cheerio api working and returning data

--- controllers/api/getArticleData.js
+++ controllers/api/getArticleData.js
@@ -2,14 +2,30 @@ const mysql = require("../../config/connection");
 const cheerio = require("cheerio");
 const axios = require('axios');
 
-
-
+const https = require('https');
 
 const url = 'https://www.espn.com/nfl/team/_/name/det/detroit-lions';
+
+
+const instance = axios.create({
+    httpsAgent: new https.Agent({  
+      rejectUnauthorized: false
+    })
+  });
+  instance.get(url);
+  
+  // At request level
+  const agent = new https.Agent({  
+    rejectUnauthorized: false
+  });
+  
+
 const articles = [];
 
 function getLinks(url){
-axios.get(url)
+
+    console.log('axios test: ----------------------------------------------------------------------------------------------------------------------------------------------------------');
+    axios.get(url, { httpsAgent: agent })
 .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
@@ -24,10 +40,12 @@ axios.get(url)
             surl
         })
 
-        console.log(`Test this: ${articles}`);
+        console.log(`Test this a test`);
     })
+
+    console.log(articles);
 })
-// return articles;
+return articles;
 
 };
 
@@ -36,5 +54,5 @@ axios.get(url)
 
 // });
 
-   
+console.log('testhing console log');
   getLinks(url);
